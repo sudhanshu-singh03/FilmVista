75f39bf5d71449dd4340d38cc22769cb40d373c2
Sudhanshu Singh <141349382+sudhanshu-singh03@users.noreply.github.com>
2024-04-30 12:35:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+document.getElementById("openModalButton1").addEventListener("click", function() {
+    var modal1 = document.querySelector(".modal1");
+    modal1.style.display = "block";
+});
+
+document.getElementById("closeModalButton1").addEventListener("click", function() {
+    var modal1 = document.querySelector(".modal1");
+    modal1.style.display = "none";
+});
+
+document.getElementById("openModalButton2").addEventListener("click", function() {
+    var modal2 = document.querySelector(".modal2");
+    modal2.style.display = "block";
+});
+
+document.getElementById("closeModalButton2").addEventListener("click", function() {
+    var modal2 = document.querySelector(".modal2");
+    modal2.style.display = "none";
+});
+
+document.getElementById("openModalButton3").addEventListener("click", function() {
+    var modal3 = document.querySelector(".modal3");
+    modal3.style.display = "block";
+});
+
+document.getElementById("closeModalButton3").addEventListener("click", function() {
+    var modal3 = document.querySelector(".modal3");
+    modal3.style.display = "none";
+});
+
+document.getElementById("openModalButton4").addEventListener("click", function() {
+    var modal4 = document.querySelector(".modal4");
+    modal4.style.display = "block";
+});
+
+document.getElementById("closeModalButton4").addEventListener("click", function() {
+    var modal4 = document.querySelector(".modal4");
+    modal4.style.display = "none";
+});
+
